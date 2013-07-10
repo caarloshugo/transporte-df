0ec274bb119f118ef07e4d0216ead1e31ea902fc
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-10 00:57:16 -0500

    view - map example

--- www/lib/themes/default/header.php
+++ www/lib/themes/default/header.php
@@ -4,16 +4,10 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title><?php print $this->getTitle(); ?></title>
 		
-		<link href="<?php print path("vendors/css/frameworks/bootstrap/bootstrap.min.css", "zan"); ?>" rel="stylesheet">
-		<link href="<?php print $this->themePath; ?>/css/style.css" rel="stylesheet">
-		
 		<script src='http://api.tiles.mapbox.com/mapbox.js/v1.0.2/mapbox.js'></script>
 		<link href='http://api.tiles.mapbox.com/mapbox.js/v1.0.2/mapbox.css' rel='stylesheet' />
 		<!--[if lte IE 8]>
 			<link href='http://api.tiles.mapbox.com/mapbox.js/v1.0.2/mapbox.ie.css' rel='stylesheet' >
 		<![endif]-->
-		
-		<?php print $this->getCSS(); ?>
 	</head>
-
 	<body>
