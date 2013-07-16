b68c0df43e92c57ccf8f99cd5c29ec91e23694ec
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-16 10:44:41 -0500

    marker icon

--- www/applications/api/views/home.php
+++ www/applications/api/views/home.php
@@ -111,13 +111,44 @@ function doAuthRedirect() {
 if($.bbq.getState('access_token')) {	
 // If there is a token in the state, consume it
 	var token = $.bbq.getState('access_token');
-	$.bbq.pushState({}, 2)
+	$.bbq.pushState({}, 2);
+	getVenues();
 } else if ($.bbq.getState('error')) {
 } else {
 	doAuthRedirect();
 }
   
-  
+
+function getVenues() {
+	$.getJSON(config.apiUrl + 'v2/venues/explore?ll=' + lat + ',' + lng + '&oauth_token=' + window.token, {}, function(data) {
+      
+      venues = data['response']['groups'][0]['items'];
+    
+      /* Place marker for each venue. */
+      for (var i = 0; i < venues.length; i++) {
+        /* Get marker's location */
+        var latLng = new L.LatLng(
+          venues[i]['venue']['location']['lat'],
+          venues[i]['venue']['location']['lng']
+        );
+        
+        /* Build icon for each icon */
+        var leafletIcon = L.Icon.extend({
+          iconUrl: venues[i]['venue']['categories'][0]['icon'],
+          shadowUrl: null,
+          iconSize: new L.Point(32,32),
+          iconAnchor: new L.Point(16, 41),
+          popupAnchor: new L.Point(0, -51)
+        });
+		
+        var marker = new L.Marker(latLng, {icon: L.icon({ iconUrl: 'images/marker-icon.png', iconSize: [25, 41], iconAnchor: [0, 0], popupAnchor: [0, -25] })})
+          .bindPopup(venues[i]['venue']['name'], { closeButton: false })
+          .on('mouseover', function(e) { this.openPopup(); })
+          .on('mouseout', function(e) { this.closePopup(); });
+        map.addLayer(marker);
+      }
+    });
+}
 
 //Mapbox	
 var map = L.mapbox.map('map', 'caarloshugo.map-1l67y9mj', { minZoom: 10, maxZoom:19, }).setView([19.41,-99.1], 12);
