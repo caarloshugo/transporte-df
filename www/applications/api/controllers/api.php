57596f7fff10d433125fead09d72563b77a0c884
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-24 22:51:10 -0500

    method search

--- www/applications/api/controllers/api.php
+++ www/applications/api/controllers/api.php
@@ -84,7 +84,7 @@ class Api_Controller extends ZP_Controller {
 	public function search($text = false) {
 		if($text and $text != "") {
 			
-			die(var_dump(urlencode($text)));
+			die(var_dump(urldecode($text)));
 			$stops = $this->Api_Model->getStopsBySearch($text);
 			
 			if(!$stops) {
