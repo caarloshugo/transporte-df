4ce24b9e1785f8fd7f8706b1c916dd29bc3ea4d4
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-08-15 17:51:31 -0500

    file - addreport

--- www/applications/api/models/api.php
+++ www/applications/api/models/api.php
@@ -245,7 +245,7 @@ class Api_Model extends ZP_Model {
 				$data["image_url"] = $upload;
 			}
 			
-			die(var_dump($upload));
+			die(var_dump($data));
 			
 			//array to postgres
 			$idStop = "{";
